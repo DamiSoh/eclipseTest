77a5f3a8d3c3c6e705a10d87eb1d913a18a66260
DamiSoh <83524438+DamiSoh@users.noreply.github.com>
2021-11-03 00:14:34 +0900

    remote add

--- test.java
+++ test.java
@@ -1,6 +1,9 @@
 class Test{
     public static void main(String[] args){
         System.out.println("hello world");
-        System.out.pringln("hello world3");
+        System.out.println("hello world3");
+
+        System.out.println("hello world4");
+        System.out.println("hello world5");
     }
 }
